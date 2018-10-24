c2a6838c33588b1f0ce46ce2294a08d811d061a6
Fan Liu <charles_liuf@hotmail.com>
2018-10-24 18:59:07 +0800

    update

---
+++ src/main/java/com/example/hello/RiffJavaHelloApplication.java
@@ -0,0 +1,19 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class RiffJavaHelloApplication {
+    @Bean
+    Function<String, String> sayHello() {
+        return f->"Hello " + f;
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiffJavaHelloApplication.class, args);
+	}
+}
