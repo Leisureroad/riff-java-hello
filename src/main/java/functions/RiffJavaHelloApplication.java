6164d8474fd95f2cf0d234baac00f4cc4d5b9bb8
Fan Liu <charles_liuf@hotmail.com>
2018-10-24 17:59:26 +0800

    upload another jar

---
+++ src/main/java/functions/RiffJavaHelloApplication.java
@@ -0,0 +1,19 @@
+package functions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class RiffJavaHelloApplication {
+	@Bean
+	Function<String, String> sayHello() {
+		return f-> "Hello "+ f;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiffJavaHelloApplication.class, args);
+	}
+}
